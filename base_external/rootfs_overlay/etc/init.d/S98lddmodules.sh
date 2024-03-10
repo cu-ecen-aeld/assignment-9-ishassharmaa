12bbb47b033605e90e2c4b418ec3fa5a6213a9bc
ishassharma <ishassharma@Ubuntu.myguest.virtualbox.org>
2024-03-10 05:43:12 +0530

    full-test sucess

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -5,14 +5,14 @@ case "$1" in
     start)
         echo "LDD loading.."
 	#startup to load the scull driver, faulty driver, modprobe hello
-	scull_load
+        scull_load
         module_load faulty
         modprobe hello
         ;;
     stop)
         echo "LDD unloading.."
 	#shutdown to unload the scull driver, faulty driver, rmmod hello
-	scull_unload
+        scull_unload
         module_unload faulty
         rmmod hello
         ;;
